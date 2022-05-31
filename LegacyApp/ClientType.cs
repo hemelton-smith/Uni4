64810276d8a262d49ca831d094c7b7c830343129
Nkululeko <bmthembu@ccint.net>
2022-05-31 17:46:52 +0200

    Refactors

---
+++ LegacyApp/ClientType.cs
@@ -0,0 +1,8 @@
+﻿namespace LegacyApp
+{
+    public static class ClientType
+    {
+        public const string VeryImportantClient = "VeryImportantClient";
+        public const string ImportantClient = "ImportantClient";
+    }
+}
---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public bool AddUser(string firname, string surname, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (string.IsNullOrEmpty(firname) || string.IsNullOrEmpty(surname) || (!email.Contains("@") && !email.Contains(".")))
+            {
+                return false;
+            }
+
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                Firstname = firname,
+                Surname = surname
+            };
+
+            DoCreditCheck(user, client.Name);
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+
+            return true;
+        }
+
+        private static void DoCreditCheck(User user, string clientName)
+        {
+
+            user.HasCreditLimit = clientName != ClientType.VeryImportantClient;
+
+            if (clientName == ClientType.VeryImportantClient)
+                return;
+
+            using (var userCreditService = new UserCreditServiceClient())
+            {
+                var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
+                creditLimit = clientName == ClientType.ImportantClient ? (creditLimit * 2) : creditLimit;
+                user.CreditLimit = creditLimit;
+            }
+        }
+    }
+}
